62a77a181b8e9912e334f96dafb1660ee4e3cef9
SimonGrchevski <simongrcevski@gmail.com>
2021-07-14 11:04:15 +0200

    Fix esLint

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
- const path = require('path');
- const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
- module.exports = {
-   entry: './src/index.js',
+module.exports = {
+  entry: './src/index.js',
 
-   plugins: [
-     new HtmlWebpackPlugin({
-       template: './src/index.html',
-     }),
-   ],
-   devServer: {
-     contentBase: './dist',
-   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  devServer: {
+    contentBase: './dist',
+  },
 
-   module: {
-     rules: [{
-       test: /\.css$/i,
-       use: ['style-loader', 'css-loader'],
-     }, ],
-   },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    }],
+  },
 
-   output: {
-     filename: 'main.js',
-     path: path.resolve(__dirname, 'dist'),
-     clean: true,
-   },
- };
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
