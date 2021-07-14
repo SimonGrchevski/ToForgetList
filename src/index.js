fb59878067f1ccc2b9a50c7ce380095ab3293779
SimonGrchevski <simongrcevski@gmail.com>
2021-07-14 08:40:52 +0200

    Add logic for sorting the array and update local storage

--- src/index.js
+++ src/index.js
@@ -8,12 +8,39 @@ class Task {
   }
 }
 
-const toDoList = [new Task('wash the dishes', false, 0), new Task('complete to to list project', false, 1)];
+let toDoList = [new Task('wash the dishes', false, 5),
+  new Task('complete to do list project', false, 1),
+  new Task('Watch movie', false, 3)
+];
 
-console.log(toDoList.length);
 
-toDoList.forEach(list => {
-  const li = document.createElement('li');
-  li.innerHTML = list.description;
-  toDoListWrapper.append(li);
-})
+sortList = () => {
+  toDoList = toDoList.sort((a, b) => a.index - b.index);
+}
+
+display = () => {
+  toDoList.forEach(list => {
+    const li = document.createElement('li');
+    li.innerHTML = `<input type='checkbox' id=${list.description} name=${list.description}>
+  <label for = ${list.description}>${list.description}</label>`
+    toDoListWrapper.append(li);
+  })
+}
+
+updateLocalStorage = () => {
+  sortList();
+  localStorage.setItem('toDoList', JSON.stringify(toDoList));
+}
+
+addTask = (newTask) => {
+  toDoList.push(newTask);
+  sortList();
+  updateLocalStorage();
+}
+
+if (JSON.parse(localStorage.getItem('toDoList'))) {
+  toDoList = JSON.parse(localStorage.getItem('toDoList'));
+  display();
+}
+
+updateLocalStorage();
